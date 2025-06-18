4799e2d7f89d75147296d28399d3c39ea8f72f3f
benanan <12416857+benanan@user.noreply.gitee.com>
2025-06-18 11:27:19 +0800

    chore(ui): 新增vite库模式，修改相关导出配置

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import {resolve} from 'path';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -10,6 +10,27 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  build: {
+    outDir: `dist/`,
+    lib: {
+      // 作为库构建时的入口
+      entry: resolve(__dirname, './src/main.ts'),
+      // 暴露的全局变量
+      name: 'lbUi',
+      // 指定库的包名
+      fileName: 'lb-ui'
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['vue'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
