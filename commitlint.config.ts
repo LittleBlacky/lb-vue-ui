16121579f970600ad421ed71d529a5290e46a9cd
benanan <12416857+benanan@user.noreply.gitee.com>
2025-07-26 23:17:36 +0800

    ci(workflow): 添加ci类型到允许的type列表

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -3,7 +3,7 @@ export default {
     rules: {
         'scope-enum': [2, 'always', ['root', 'components', 'docs', 'utils', 'theme-chalk', 'core', 'play', 'workflow']], // 允许的 scope 列表
         'scope-empty': [2, 'never'], // 强制要求 scope
-        'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']], // 允许的 type 列表
+        'type-enum': [2, 'always', ['ci', 'feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']], // 允许的 type 列表
         'type-empty': [2, 'never'], // 强制要求 type
     }
 };
