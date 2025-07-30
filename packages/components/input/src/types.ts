c754ab016d1dac889ef82e241fb221ea1320b35c
LittleBlacky <805399080@qq.com>
2025-07-30 22:15:26 +0800

    feat(components): 完善input功能及其样式

--- packages/components/input/src/types.ts
+++ packages/components/input/src/types.ts
@@ -1,7 +1,31 @@
-export interface LbInputProps {
-  // 定义组件的属性
-  // 例如: text?: string;
+export interface LbInputAutoSize {
+  minRows?: number;
+  maxRows?: number;
 }
+
+export interface LbInputProps {
+  modelValue?: string | number;
+  type?: "text" | "password" | "textarea" | "number";
+  placeholder?: string;
+  disabled?: boolean;
+  clearable?: boolean;
+  showPassword?: boolean;
+  prefixIcon?: string;
+  autofocus?: boolean;
+  autosize?: boolean | LbInputAutoSize;
+  suffixIcon?: string;
+  size?: "large" | "default" | "small";
+  rows?: number;
+  cols?: number;
+  maxlength?: number;
+  minlength?: number;
+  showWordLimit?: boolean;
+}
+
 export interface LbInputEmits {
-  // (e: 'click', event: MouseEvent): void;
+  (e: "update:modelValue", value: string): void;
+  (e: "change", value: string): void;
+  (e: "focus", event: FocusEvent): void;
+  (e: "blur", event: FocusEvent): void;
+  (e: "clear"): void;
 }
