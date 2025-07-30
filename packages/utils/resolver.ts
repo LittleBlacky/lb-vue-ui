995b7266554110e5e2ab3d84e23d00a2b8eb5541
LittleBlacky <805399080@qq.com>
2025-07-30 21:48:21 +0800

    fix(utils): 修复resolver无法正确解析组件及其样式问题

--- packages/utils/resolver.ts
+++ packages/utils/resolver.ts
@@ -1,63 +1,65 @@
 // my-resolver.ts
-import type { ComponentResolverFunction } from 'unplugin-vue-components/types'
+import type { ComponentResolverFunction } from "unplugin-vue-components/types";
 
 export interface LbsolverOptions {
   /**
    * 是否自动导入组件（默认 true）
    */
-  autoImportComponent?: boolean
+  autoImportComponent?: boolean;
   /**
    * 是否自动导入样式（默认 true）
    */
-  autoImportStyle?: boolean | 'css' | 'sass'
+  autoImportStyle?: boolean | "css" | "sass";
   /**
    * 组件库前缀
    */
-  prefix?: string
+  prefix?: string;
   /**
    * 组件源路径
    */
-  libraryName?: string
+  libraryName?: string;
   /**
    * 样式文件基础路径
    */
-  stylePath?: string
+  stylePath?: string;
 }
 
-export const LbResolver = (options: LbsolverOptions = {}): ComponentResolverFunction => {
+export const LbResolver = (
+  options: LbsolverOptions = {}
+): ComponentResolverFunction => {
   const {
     autoImportStyle = true,
-    prefix = 'Lb', // 组件前缀
-    libraryName = '@lb-vue-ui', // 组件库名称
-    stylePath = `${libraryName}/theme-chalk`, //样式文件路径
+    prefix = "Lb", // 组件前缀
+    libraryName = "@littleblacky/lb-vue-ui", // 组件库名称
+    stylePath = `${libraryName}/style`, //样式文件路径
   } = options;
   return (name: string) => {
-    if (prefix && !name.startsWith(prefix)) return undefined
+    if (prefix && !name.startsWith(prefix)) return undefined;
     const tagName = prefix ? name.slice(prefix.length).toLowerCase() : name;
     const ComponentName = name;
     // 返回的解析结果
     const result: {
-      type: 'component';
+      type: "component";
       as?: string;
       name?: string;
       from: string;
       sideEffects?: string | string[];
     } = {
-      type: 'component',
+      type: "component",
       from: libraryName,
       as: ComponentName,
-      name: ComponentName
+      name: ComponentName,
     };
     // 1. 处理样式导入
     if (autoImportStyle && stylePath) {
-      const styleExt =
-        autoImportStyle ? 'css' :
-          autoImportStyle === 'sass' ? 'scss' : 'css';
+      const styleExt = autoImportStyle
+        ? "css"
+        : autoImportStyle === "sass"
+          ? "scss"
+          : "css";
 
-      result.sideEffects = [
-        `${stylePath}/${tagName}.${styleExt}`,
-      ];
+      result.sideEffects = [`${stylePath}/${tagName}.${styleExt}`];
     }
     return Object.keys(result).length > 0 ? result : undefined;
   };
-}
+};
