f4dca885567c2de15322fdffa6dee584123b4e26
LittleBlacky <805399080@qq.com>
2025-08-02 12:07:48 +0800

    fix(demos): 更新卡片组件底部按钮样式，使用flex布局对齐

--- docs/src/components/data/card/demos/footerSlot.vue
+++ docs/src/components/data/card/demos/footerSlot.vue
@@ -2,7 +2,7 @@
   <lb-card header="卡片标题">
     <p>卡片内容</p>
     <template #footer>
-      <div style="text-align: right">
+      <div style="text-align: right; display: flex; justify-content: flex-end">
         <lb-button size="small">取消</lb-button>
         <lb-button size="small" type="primary" style="margin-left: 8px"
           >确定</lb-button
