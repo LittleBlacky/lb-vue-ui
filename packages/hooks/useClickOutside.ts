a663252764443e14f89be30a2cba20e7d9f53161
LittleBlacky <805399080@qq.com>
2025-08-04 10:11:37 +0800

    feat(hooks): 新增响应点击外部的hooks

---
+++ packages/hooks/useClickOutside.ts
@@ -0,0 +1,20 @@
+import { onBeforeUnmount, onMounted, type Ref } from "vue";
+
+export default function useClickOutside(
+  elementTarget: Ref<undefined | HTMLElement>,
+  callback: (e: MouseEvent) => void
+) {
+  const handler = (e: MouseEvent) => {
+    if (elementTarget.value && e.target) {
+      if (!elementTarget.value.contains(e.target as HTMLElement)) {
+        callback(e);
+      }
+    }
+  };
+  onMounted(() => {
+    document.addEventListener("click", handler);
+  });
+  onBeforeUnmount(() => {
+    document.removeEventListener("click", handler);
+  });
+}
