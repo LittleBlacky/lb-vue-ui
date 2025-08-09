ceffe8095b7c4a1c5aeba67a814583e2bedfed2a
LittleBlacky <805399080@qq.com>
2025-08-09 23:33:42 +0800

    chore: release v1.2.4

--- play/src/App.vue
+++ play/src/App.vue
@@ -1,9 +1,35 @@
 <template>
-  <div style="display: flex; gap: 10px">
-    <LbButton disabled>Disabled Button</LbButton>
-    <LbButton type="primary" disabled>Disabled Primary</LbButton>
-    <LbButton type="primary" disabled plain>Disabled Primary</LbButton>
-    <LbButton type="danger" disabled link>Disabled Primary</LbButton>
-    <LbButton type="warning" disabled text>Disabled Primary</LbButton>
+  <div class="virtual-triggering-demo">
+    <lb-button
+      v-for="i in 2"
+      :key="i"
+      class="virtual-trigger"
+      @mousemove="(e) => (virtualRef = e.currentTarget)"
+      @click="visible = !visible"
+    >
+      Click me {{ i }}
+    </lb-button>
+    <lb-tooltip
+      content="Hello Virtual Trigger"
+      virtualTriggering
+      :visible="visible"
+      :virtualRef="virtualRef"
+    ></lb-tooltip>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const visible = ref(false)
+const virtualRef = ref<HTMLElement | undefined>()
+</script>
+
+<style scoped>
+.virtual-triggering-demo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  height: 100px;
+}
+</style>
